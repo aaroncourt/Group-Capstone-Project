4c5e54bf20b52719b355cc57c0ef49c087369d86
Paul Morgan <morganducks@gmail.com>
2022-05-19 08:09:39 -0700

    paul-5-19-2

---
+++ client/src/components/Main.js
@@ -0,0 +1,18 @@
+
+
+
+
+const Main = (props) => {
+ 
+
+
+    //loop through records and add all ideaLikes
+
+    return (
+       <div></div>
+
+    )
+
+}
+
+export default Main;
---
+++ client/src/components/UserPosts.js
@@ -0,0 +1,18 @@
+
+
+
+
+const UserPosts = (props) => {
+ 
+
+
+    //loop through records and add all ideaLikes
+
+    return (
+       <div></div>
+
+    )
+
+}
+
+export default UserPosts;
