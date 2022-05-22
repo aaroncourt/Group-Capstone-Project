5eb445be9f10784ca1af394dd104c256d118b6b4
Lagomorph <godzilka@gmail.com>
2022-05-21 23:41:14 -0700

    Main, Add, View checking
    This is the first pass at these pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Main from "./components/Main"
 import OnePost from "./components/OnePost"
 import UserPosts from "./components/UserPosts"
@@ -7,6 +8,20 @@ import Replies from "./components/Replies"
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
+=======
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main/>} />
+            <Route path="/new" element={<OnePost/>} />
+            <Route path="/reply/:id" element={<Replies/>} />
+            <Route path="/user/:id" element={<UserPosts/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
