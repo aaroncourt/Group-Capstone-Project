7b5661a61ffc0334d0add5cb978b1fe808f8d05a
Mohammed <mohd.ntw@gmail.com>
2022-05-22 19:56:16 -0600

    login - logout - current Logged in user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,8 @@
 const User = require('../models/user.model');
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+
+
 
 module.exports = {
 
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,14 +1,14 @@
 const postController = require('../controllers/post.controller');
 const upload = require('../config/multer.config')
-
+const { authenticate } = require('../config/jwt.config');
 
 module.exports = (app) => {
-    app.get('/api/posts/all', postController.getAllPosts);
-    app.get('/api/posts/all/:id', postController.getAllPostsByUserId);
-    app.post('/api/posts/new',upload.single('postPicture'), postController.createPost);
-    app.get('/api/posts/:id', postController.getOnePost);
-    app.put('/api/posts/:id/edit', postController.updatePost);
-    app.delete('/api/posts/:id', postController.deletePost);
-    app.put('/api/post/addimage/:id',upload.single('postPicture'), postController.addImage);
+    app.get('/api/posts/all',authenticate,  postController.getAllPosts);
+    app.get('/api/posts/all/:id',authenticate,  postController.getAllPostsByUserId);
+    app.post('/api/posts/new',authenticate, upload.single('postPicture'), postController.createPost);
+    app.get('/api/posts/:id',authenticate,  postController.getOnePost);
+    app.put('/api/posts/:id/edit',authenticate,  postController.updatePost);
+    app.delete('/api/posts/:id',authenticate,  postController.deletePost);
+    app.put('/api/post/addimage/:id',authenticate, upload.single('postPicture'), postController.addImage);
 
 };
